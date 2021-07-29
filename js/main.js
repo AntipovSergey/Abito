963ae1e3686043811c20455577ceff1b197ce70e
Sergey Antipov <72650361+AntipovSergey@users.noreply.github.com>
2021-07-29 12:33:56 +0300

    Выполняет задание 4-го урока

--- js/main.js
+++ js/main.js
@@ -11,3 +11,14 @@ let swiper2 = new Swiper(".mySwiper2", {
       swiper: swiper,
     },
 });
+
+const menuBtn = document.querySelector('.menu-button');
+const menuClose = document.querySelector('.close-menu');
+const menu = document.querySelector('.navbar-pannel');
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('is-open');
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.toggle('is-open');
+});
